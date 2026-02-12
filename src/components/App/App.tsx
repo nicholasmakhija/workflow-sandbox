86b98094617085b78ed417130ddf08fe4d6f05b8
Nicholas Makhija <nicholasmakhija@gmail.com>
2026-02-13 10:56:15 +1100

    refactor: added log in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
 import { HelloWorld } from '../HelloWorld';
 
 export const App = () => {
+  useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.log('App mounted');
+  }, []);
+
   return (
     <div className="App">
       <HelloWorld>Hello World</HelloWorld>
