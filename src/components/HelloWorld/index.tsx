9316f4ff19e5b2b8908c5b2b1e7f4af5b9d7548e
Nicholas Makhija <nicholasmakhija@gmail.com>
2026-02-13 16:58:58 +1100

    refactor: HelloWorld component styling

--- src/components/HelloWorld/index.tsx
+++ src/components/HelloWorld/index.tsx
@@ -6,8 +6,23 @@ export const HelloWorld = ({
   children
 }: HelloWorldProps) => {
   return (
-    <div className="HelloWorld">
-      <h1>{ children }</h1>
+    <div
+      className="HelloWorld"
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: '0 auto',
+        padding: '24px 16px',
+        maxWidth: '1140px',
+        height: '100%'  
+      }}
+    >
+      <h1
+        style={{
+          color: 'hotpink'
+        }}
+      >{ children }</h1>
     </div>
   );
 };
